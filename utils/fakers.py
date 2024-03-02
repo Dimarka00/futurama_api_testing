b7719a10547fe3ec0ab09b95a982be470152d698
Dima Klimko <dimadimarkinn@gmail.com>
2024-03-03 02:03:48 +0300

    Добавлена генерация случайных данных

--- utils/fakers.py
+++ utils/fakers.py
@@ -10,9 +10,9 @@ def random_number_from_1_to_100(start: int = 1, end: int = 100) -> int:
     return randint(start, end)
 
 
-def random_string(start: int = 10, end: int = 50) -> str:
+def random_string(start: int = 5, end: int = 15) -> str:
     return ''.join(choice(ascii_letters + digits) for _ in range(randint(start, end)))
 
 
-def random_list_of_strings(start: int = 10, end: int = 50) -> list[str]:
+def random_list_of_strings(start: int = 5, end: int = 15) -> list[str]:
     return [random_string() for _ in range(randint(start, end))]
